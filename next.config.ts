b796bfa4808cf981bee42d9b3e6cd6c192e8c2f8
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2025-10-25 12:39:43 -0700

    Switch to webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Mark ffmpeg and ffprobe installers as external to prevent bundling
+      config.externals = config.externals || [];
+      config.externals.push({
+        "@ffmpeg-installer/ffmpeg": "commonjs @ffmpeg-installer/ffmpeg",
+        "@ffprobe-installer/ffprobe": "commonjs @ffprobe-installer/ffprobe",
+      });
+    }
+    return config;
+  },
+  serverExternalPackages: [
+    "@ffmpeg-installer/ffmpeg",
+    "@ffprobe-installer/ffprobe",
+    "sharp",
+  ],
 };
 
 export default nextConfig;
